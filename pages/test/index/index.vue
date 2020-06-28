70dbe926a8a5d6028da5355bc3897fce56417175
caiwang0921 <505673793@qq.com>
2020-06-28 09:19:34 +0800

    uni-app上传github

---
+++ pages/test/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+			<text>haha444</text>
+		</view>
+	
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+   .logo{
+	   width: 150px;
+	   height: 150px;
+   }
+</style>
